27044be2f92be243dff6501cbe31e5e924e6bd20
wyjeon <wyjeon.info@gmail.com>
2021-03-14 15:55:16 +0900

    스타일 설정

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 function PostListPage() {
-  return <div>PostListPage</div>;
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
 }
 
 export default PostListPage;
